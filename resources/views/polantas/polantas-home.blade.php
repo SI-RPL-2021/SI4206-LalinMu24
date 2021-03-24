9ae01cc4aefe7b2c369f67c184afda16ae2169bc
hagaitrg <disahagait@gmail.com>
2021-03-24 08:24:39 +0700

    fix bug denah bandung ref #1

--- resources/views/polantas/polantas-home.blade.php
+++ resources/views/polantas/polantas-home.blade.php
@@ -11,7 +11,9 @@
             <!-- Card Content - Collapse -->
             <div class="collapse show" id="collapseCardExample">
                 <div class="card-body">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63374.28122706833!2d107.60805220644426!3d-6.903448166706584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e6398252477f%3A0x146a1f93d3e815b2!2sBandung%2C%20Bandung%20City%2C%20West%20Java!5e0!3m2!1sen!2sid!4v1616541815814!5m2!1sen!2sid" width="900" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63374.28122706833!2d107.60805220644426!3d-6.903448166706584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e6398252477f%3A0x146a1f93d3e815b2!2sBandung%2C%20Bandung%20City%2C%20West%20Java!5e0!3m2!1sen!2sid!4v1616541815814!5m2!1sen!2sid" allowfullscreen></iframe>
+                    </div>
                 </div>
             </div>
         </div>
