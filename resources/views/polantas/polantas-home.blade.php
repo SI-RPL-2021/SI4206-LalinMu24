75947eed557ee96c0126f93a8908476f4703dc3e
lanangbp <bplanang@gmail.com>
2021-03-24 09:15:53 +0700

    Menambahkan deskripsi peta ref #1

--- resources/views/polantas/polantas-home.blade.php
+++ resources/views/polantas/polantas-home.blade.php
@@ -14,6 +14,8 @@
                     <div class="embed-responsive embed-responsive-16by9">
                         <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63374.28122706833!2d107.60805220644426!3d-6.903448166706584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e6398252477f%3A0x146a1f93d3e815b2!2sBandung%2C%20Bandung%20City%2C%20West%20Java!5e0!3m2!1sen!2sid!4v1616541815814!5m2!1sen!2sid" allowfullscreen></iframe>
                     </div>
+                    <br>
+                    <p>Peta perempatan pasteur dengan detail perempatan beruba jalan persimpangan dan informasi lalu lintas </p>
                 </div>
             </div>
         </div>
