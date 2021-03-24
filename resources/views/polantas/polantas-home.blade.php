a71c1386ba17eccf07c9ed5637dfff0a81f9e690
jtamariska <jiletamamriska@gmail.com>
2021-03-24 09:38:12 +0700

    menambahkan live video

--- resources/views/polantas/polantas-home.blade.php
+++ resources/views/polantas/polantas-home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="row">
-    <div class="col-md-7">
+    <div class="col-md-6">
         <div class="card shadow mb-4">
             <!-- Card Header - Accordion -->
             <a href="#collapseCardExample" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="collapseCardExample">
@@ -11,10 +11,27 @@
             <!-- Card Content - Collapse -->
             <div class="collapse show" id="collapseCardExample">
                 <div class="card-body">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63374.28122706833!2d107.60805220644426!3d-6.903448166706584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e6398252477f%3A0x146a1f93d3e815b2!2sBandung%2C%20Bandung%20City%2C%20West%20Java!5e0!3m2!1sen!2sid!4v1616541815814!5m2!1sen!2sid" width="900" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63374.28122706833!2d107.60805220644426!3d-6.903448166706584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e6398252477f%3A0x146a1f93d3e815b2!2sBandung%2C%20Bandung%20City%2C%20West%20Java!5e0!3m2!1sen!2sid!4v1616541815814!5m2!1sen!2sid" width="900" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    <div class="col-md-6">
+        <div class="card shadow mb-4">
+            <!-- Card Header - Accordion -->
+            <a href="#collapseCardExample" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="collapseCardExample">
+                <h6 class="m-0 font-weight-bold text-primary">Live video</h6>
+            </a>
+            <!-- Card Content - Collapse -->
+            <div class="collapse show" id="collapseCardExample">
+                <div class="card-body">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/aumaiyWSpYE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
+
     </div>
 </div>
 @endsection
